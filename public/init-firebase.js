106098bdee9b1685adea71a450183fee3f1aed2f
Daniela Flores <diflores@uc.cl>
2020-05-11 00:15:37 -0400

    Add firebase basic configuration

---
+++ public/init-firebase.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBCm_PbiKi8Yrjhlh5MfO-_T39ZzRzm7L8",
+  authDomain: "jspotify-3aac1.firebaseapp.com",
+  databaseURL: "https://jspotify-3aac1.firebaseio.com",
+  projectId: "jspotify-3aac1",
+  storageBucket: "jspotify-3aac1.appspot.com",
+  messagingSenderId: "405634801275",
+  appId: "1:405634801275:web:d4c2f62aac8a23d4de0cde",
+  measurementId: "G-6722PQ4PL0"
+};
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
