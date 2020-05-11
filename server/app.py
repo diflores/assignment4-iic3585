04e7452ad1a54d4d90cec2e34e0c7e9f78a4945f
Daniela Flores <diflores@uc.cl>
2020-05-11 18:31:14 -0400

    Finish v1 of songs api

---
+++ server/app.py
@@ -0,0 +1,28 @@
+import glob
+
+from flask import Flask, request, send_from_directory, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route("/")
+def hello_world():
+    return jsonify({"Hello": "World"})
+
+
+@app.route("/songs/")
+def get_song():
+    song_name = request.args.get("song_name")
+    return send_from_directory("./songs",
+                               song_name, as_attachment=False)
+
+
+@app.route("/song_list")
+def get_song_list():
+    return jsonify([song.split("/")[-1] for song in glob.glob("./songs/*.mp3")])
+
+
+if __name__ == '__main__':
+    app.run(port=8080)
