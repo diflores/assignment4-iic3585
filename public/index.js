aa1073047dc8225f718f67f89f6fa62135c66a65
Daniela Flores <diflores@uc.cl>
2020-05-11 18:19:49 -0400

    Add cache support

--- public/index.js
+++ public/index.js
@@ -1,27 +1,78 @@
-const db = firebase.firestore();
+const songList = document.getElementById("song-list");
 
-db.collection("music-styles")
-  .get()
-  .then(snapshot => {
-    snapshot.forEach(doc => {
-      const styleSelector = document.getElementById("styleSelector");
-      const option = document.createElement("option");
-      option.text = doc.data()["style"];
-      if (option.text === "pop") {
-        option.setAttribute("selected", true);
-      }
-      option.value = doc.data()["url"];
-      styleSelector.add(option);
+// Fetch Songs from local server
+fetch("http://127.0.0.1:8080/song_list")
+  .then(response => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error(response.statusText);
+    }
+  })
+  .then(data => {
+    data.forEach(song => {
+      const li = document.createElement("li");
+      const songURL = `http://127.0.0.1:8080/songs/?song_name=${song}`;
+      li.innerHTML = `<a data-song href="${songURL}">&#9654; Play ${song}</a> <i class="fas fa-heart to-be-saved" onclick="likeSong(event, '${songURL}')"></i>`;
+
+      songList.appendChild(li);
     });
   })
-  .catch(err => {
-    console.log("Error getting documents", err);
+  .then(() => {
+    // Create playlist
+    // https://gomakethings.com/a-simple-progressively-enhanced-audio-player-with-vanilla-js/
+    Array.prototype.forEach.call(
+      document.querySelectorAll("[data-song]"),
+      function (song) {
+        console.log(song);
+        // Create a new Audio object for the song
+        song.audio = new Audio(song.href);
+
+        // Add a11y attributes
+        song.setAttribute("role", "button");
+        song.setAttribute("aria-pressed", "false");
+      }
+    );
+
+    document.addEventListener(
+      "click",
+      function (event) {
+        // Ignore clicks on elements that aren't the song link
+        if (!event.target.hasAttribute("data-song")) return;
+
+        // Prevent link default
+        event.preventDefault();
+
+        // If the item is already playing, hit pause
+        if (event.target.getAttribute("aria-pressed") == "true") {
+          event.target.audio.pause();
+          event.target.setAttribute("aria-pressed", "false");
+          return;
+        }
+
+        // Otherwise, play it
+        event.target.audio.play();
+        event.target.setAttribute("aria-pressed", "true");
+      },
+      false
+    );
   });
 
-const changeMusicStyle = () => {
-  const styleSelector = document.getElementById("styleSelector");
-  const selectedOption = styleSelector.options[styleSelector.selectedIndex].value;
-  const musicPlayer = document.getElementById("music-style-player");
-  musicPlayer.setAttribute("src", selectedOption);
-};
-console.log("HO");
+function likeSong(event, songURL) {
+  if (event.target.style.color !== "red") {
+    // Style heart
+    event.target.style.color = "red";
+    event.target.setAttribute("song-url", songURL);
+    event.target.classList.add("to-be-saved");
+    event.target.classList.add("saved");
+    // Save song in cache
+    caches.open("pages-cache-v1").then(function (cache) {
+      return cache.addAll([songURL]);
+    });
+  } else {
+    // Back to original style
+    event.target.style.color = "black";
+    event.target.classList.remove("saved");
+    event.target.classList.add("to-be-saved");
+  }
+}
--- public/service-worker.js
+++ public/service-worker.js
@@ -2,10 +2,8 @@ const cacheName = "pages-cache-v1";
 
 const filesToCache = ["./", "./index.html", "./style.css"];
 self.addEventListener("install", function (e) {
-  console.log("[ServiceWorker] Install");
   e.waitUntil(
     caches.open(cacheName).then(function (cache) {
-      console.log("[ServiceWorker] Caching app shell");
       return cache.addAll(filesToCache);
     })
   );
